8bcf5a79d752532af5224098ace7f41699b76050
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2021-06-21 13:01:52 +0900

    Create 이상한 문자 만들기.java

---
+++ Programmers/Level 1/이상한 문자 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int idx=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) != ' '){
+                if(idx++%2==0){
+                    answer += Character.toUpperCase(s.charAt(i));
+                }else{
+                    answer += Character.toLowerCase(s.charAt(i));
+                }
+            }else{
+                idx = 0;
+                answer += " ";
+            }
+        }
+        return answer;
+    }
+}
