a9d49c50f064e57ad00f4cd25f759e8e33b4c74c
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2022-01-22 21:27:52 +0900

    Create 소수 만들기.java

---
+++ Programmers/Level 1/소수 만들기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+    
+    public boolean isPrime(int n){
+        if(n == 1) return false;
+        if(n == 2) return true;
+        for(int i=2; i<=(int)Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
