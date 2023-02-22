a29f02b0befced29c74e33edd1dc1e876ddaa8a7
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2023-02-22 21:32:20 +0900

    [Gold V] Title: 치킨 배달, Time: 204 ms, Memory: 15040 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,78 +1,89 @@
-import java.awt.Point;
+import java.awt.*;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-    static int n, m, ans;
-    
-	public static void main(String[] args) throws IOException {
+
+    // NxN 도시
+    // 1 : 집, 2 : 치킨집
+    // 치킨집 개수 : 최대 M개
+    // 모든 집의 치킨 거리의 합이 최소
+
+    static int N, M, ans;
+    static int[][] map;
+    static ArrayList<Point> chickens;
+    static ArrayList<Point> homes;
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        
-        ArrayList<Point>  chi = new ArrayList<>();
-        ArrayList<Point> homes = new ArrayList<>();
-        
-        for(int i=0; i<n; i++) {
-        	st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++) {
-                int item = Integer.parseInt(st.nextToken());
-                if(item == 2) { // 치킨집이면
-                    chi.add(new Point(i, j));
-                }else if(item == 1) {
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N+1][N+1];
+        chickens = new ArrayList<>();
+        homes = new ArrayList<>();
+        ans = Integer.MAX_VALUE;
+
+        for(int i=1; i<=N; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j=1; j<=N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 1){
                     homes.add(new Point(i, j));
+                }else if(map[i][j] == 2){
+                    chickens.add(new Point(i, j));
                 }
             }
         }
-        
-        ans = Integer.MAX_VALUE;
-        
-        comb(chi.size(), new int[m], 0, 0, chi, homes);
-        
-        bw.write(String.valueOf(ans));
-        
-        br.close();
-        bw.close();
-	}
 
-	
-    public static void comb(int arrlen, int[] sel, int idx, int k, ArrayList<Point> chi, ArrayList<Point> homes) {
-        if(k == m) {
-        	int tempans = 0;
-        	for(int i=0; i<homes.size(); i++) { // 집들
-        		int dist = Integer.MAX_VALUE;
-        		int hx = homes.get(i).x;
-        		int hy = homes.get(i).y;        
-        		
-        		for(int s : sel) { // 고른 치킨집
-	            	
-	            	int cx = chi.get(s).x;
-	            	int cy = chi.get(s).y;
-            	
-            		int temp = Math.abs(cx - hx) + Math.abs(cy - hy);
-            		
-            		dist = Math.min(dist, temp);
-            	}
-        		tempans += dist;
+        comb(chickens.size(), new int[M], 0, 0);
+
+        System.out.println(ans);
+    }
+
+    private static void comb(int size, int[] output, int idx, int k) {
+
+        if (k == M) {
+//            System.out.println(Arrays.toString(output));
+
+            // M개 치킨집 골랐다.
+
+            // 이제 각 집의 치킨거리를 구한다.
+            int tempans = 0;
+
+            // 집을 돌면서
+            for(Point home : homes){
+                // 이 집의 최소 치킨거리를 찾기 위해 비교할 값을 담는 변수
+                int temp = Integer.MAX_VALUE;
+
+                // 고른 치킨집을 돌면서
+                for(int o : output){
+                    temp = Math.min(temp, calDist(home, chickens.get(o)));
+                }
+
+                tempans += temp;
             }
-        	ans = Math.min(ans, tempans);
-            
+
+            ans = Math.min(ans, tempans);
+
             return;
         }
-        
-        for(int i=idx; i<chi.size(); i++) {
-            sel[k] = i;
-            comb(arrlen, sel, i+1, k+1, chi, homes);
+
+        for(int i=idx; i<size; i++){
+            output[k] = i;
+            comb(size, output, i+1, k+1);
         }
     }
+
+    private static int calDist(Point a, Point b){
+        return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+    }
+
 }
