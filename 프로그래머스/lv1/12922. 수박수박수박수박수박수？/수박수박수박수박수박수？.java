c258e1931860c877acecd78b7d5eb1216b78ab2f
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2023-05-24 09:15:09 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.07 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=n; i++){
+            sb.append(i%2==1 ? "수" : "박");
+        }
+        return sb.toString();
+    }
+}
