9b7fd26762cc53c7086cdd07afc1fe18749d71be
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2023-05-24 10:24:05 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String s) {
+        
+        if(s.length() != 4 && s.length() != 6) return false;
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) - '0' < 0 || s.charAt(i) - '0' > 9) return false;
+        }
+        
+        return true;
+    }
+}
