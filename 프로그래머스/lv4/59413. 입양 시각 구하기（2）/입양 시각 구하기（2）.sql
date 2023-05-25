4aa1f2b11fdefd08cd667ddea953463d7946c812
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2023-05-25 10:46:48 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,25 +1,18 @@
-# with recursive monthly as(
-#     select 1 as rnum
-#     union all
-#     select rnum + 1 from monthly
-#     where rnum < 23
-# )
-# select rnum from monthly;
-
-SELECT rnum as HOUR, IFNULL(COUNT, 0) AS COUNT
-from 
-(SELECT hour(datetime) as HOUR, count(*) as COUNT 
- from animal_outs
- group by hour(datetime)
-) a 
- right outer join 
- (
-    with recursive monthly as(
-        select 0 as rnum
-        union all
-        select rnum + 1 from monthly
-        where rnum < 23
-    )
-    select rnum from monthly
-) b
-on a.hour = b.rnum
+with recursive hourly as(
+    select 0 as rnum
+    union all
+    select rnum+1 
+    from hourly
+    where rnum < 23
+)
+select rnum as HOUR, IFNULL(COUNT,0) as COUNT
+# select *
+from hourly h 
+left outer join
+(
+    select hour(datetime) as hour, count(*) as COUNT
+    from animal_outs
+    group by hour
+) a
+on(h.rnum = a.hour)
+# group by rnum
