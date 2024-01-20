b065212f32ff78c420fdf3f40b67dbc84837ef68
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2024-01-20 21:33:28 +0900

    [level 3] Title: 여행경로, Time: 10.03 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,71 +1,34 @@
 import java.util.*;
-/*
-[a,b]
-1. a 다음 b로 정렬
-2. visited 배열
-
-*/
 class Solution {
     boolean[] visited;
-    ArrayList<String> output;
-    boolean end = false;
-    String[] answer;
+    ArrayList<String> list;
     public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        visited = new boolean[tickets.length];
+        list = new ArrayList<>();
         
-        // a 다음 b 순서로 정렬
-        Arrays.sort(tickets, new Comparator<>(){
-            public int compare(String[] ticket1, String[] ticket2){
-                if(ticket1[0].equals(ticket2[0])){
-                    return ticket1[1].compareTo(ticket2[1]);
-                }
-                return ticket1[0].compareTo(ticket2[0]);
-            }
-        });
+        dfs(tickets, 0, tickets.length, "ICN", "ICN");
         
-        for(int i=0; i<tickets.length; i++){
-            if(tickets[i][0].equals("ICN")){
-                visited = new boolean[tickets.length];
-                output = new ArrayList<>();
-                
-                output.add("ICN");
-                visited[i] = true;
-                dfs(tickets, "ICN", tickets[i][1], 1, i, tickets.length);
-                if(end) break;
-            }
-        }
+        Collections.sort(list);
         
-        return answer;
+        return list.get(0).split(" ");
     }
     
-    void dfs(String[][] tickets, String from, String to, int depth, int idx, int n){
-        visited[idx] = true;
+    public void dfs(String[][] tickets, int depth, int n, String from, String route){
         
-        if(end) return; // 이미 경로를 완성했으면 끝
-        if(isVisitedAll(visited)){ // 모든 도시를 방문했으면 경로 완성
-            output.add(to); 
-            end = true;
-            answer = new String[output.size()];
-            answer = output.toArray(answer);
+        if(depth == n){
+            list.add(route);
             return;
         }
         
         for(int i=0; i<tickets.length; i++){
             if(visited[i]) continue;
             
-            if(tickets[i][0].equals(to)){
-                output.add(to);
-                dfs(tickets, to, tickets[i][1], depth+1, i, n);
-                output.remove(output.size()-1);
+            if(tickets[i][0].equals(from)){
+                visited[i] = true;
+                dfs(tickets, depth+1, n, tickets[i][1], route + " " + tickets[i][1]);
+                visited[i] = false;
             }
         }
-        visited[idx] = false;
-    }
-    
-    boolean isVisitedAll(boolean[] visited){ // 모든 도시를 방문했는가
-        for(int i=0; i<visited.length; i++){
-            if(!visited[i])
-                return false;
-        }
-        return true;
     }
 }
