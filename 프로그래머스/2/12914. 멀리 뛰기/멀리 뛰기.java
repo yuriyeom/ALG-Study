9dfb5dcbd7a30793b8a02027c0040053dd692d1b
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2024-01-27 14:37:20 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    long answer = 0;
+    public long solution(int n) {
+        
+        long[] d = new long[2001];
+        
+        d[1] = 1;
+        d[2] = 2;
+        
+        for(int i=3; i<=2000; i++){
+            d[i] = (d[i-1] + d[i-2]) % 1234567;
+        }
+        
+        return d[n];
+    }
+}
