7dd18f8d6c433744ef34c837e9e5f462da223500
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2024-02-02 12:50:56 +0900

    [Gold V] Title: 치킨 배달, Time: 160 ms, Memory: 13076 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -7,83 +7,64 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    // NxN 도시
-    // 1 : 집, 2 : 치킨집
-    // 치킨집 개수 : 최대 M개
-    // 모든 집의 치킨 거리의 합이 최소
-
-    static int N, M, ans;
+    static int N, M, ans=Integer.MAX_VALUE;
     static int[][] map;
-    static ArrayList<Point> chickens;
-    static ArrayList<Point> homes;
+    static ArrayList<Point> houseList, chickenList;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken()); // NxN map
+        M = Integer.parseInt(st.nextToken()); // 유효한 치킨집 수
 
-        map = new int[N+1][N+1];
-        chickens = new ArrayList<>();
-        homes = new ArrayList<>();
-        ans = Integer.MAX_VALUE;
+        houseList = new ArrayList<>();
+        chickenList = new ArrayList<>();
 
-        for(int i=1; i<=N; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j=1; j<=N; j++){
+        map = new int[N][N];
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 1){
-                    homes.add(new Point(i, j));
-                }else if(map[i][j] == 2){
-                    chickens.add(new Point(i, j));
-                }
+                if(map[i][j] == 1) houseList.add(new Point(i, j));
+                else if(map[i][j] == 2) chickenList.add(new Point(i, j));
             }
         }
 
-        comb(chickens.size(), new int[M], 0, 0);
+        // 각 집마다 가장 가까운 치킨집과의 거리
+        pickChicken(0, 0, new boolean[chickenList.size()], new int[M]);
 
         System.out.println(ans);
     }
 
-    private static void comb(int size, int[] output, int idx, int k) {
-
-        if (k == M) {
-//            System.out.println(Arrays.toString(output));
-
-            // M개 치킨집 골랐다.
-
-            // 이제 각 집의 치킨거리를 구한다.
-            int tempans = 0;
-
-            // 집을 돌면서
-            for(Point home : homes){
-                // 이 집의 최소 치킨거리를 찾기 위해 비교할 값을 담는 변수
-                int temp = Integer.MAX_VALUE;
-
-                // 고른 치킨집을 돌면서
-                for(int o : output){
-                    temp = Math.min(temp, calDist(home, chickens.get(o)));
-                }
-
-                tempans += temp;
-            }
-
-            ans = Math.min(ans, tempans);
-
+    public static void pickChicken(int idx, int depth, boolean[] visited, int[] output){
+        if(depth == M){ // 치킨집 M개 선택했으면
+            calDistance(output);
             return;
         }
 
-        for(int i=idx; i<size; i++){
-            output[k] = i;
-            comb(size, output, i+1, k+1);
+        for(int i=idx; i<chickenList.size(); i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            output[depth] = i;
+            pickChicken(i+1, depth+1, visited, output);
+            visited[i] = false;
         }
     }
 
-    private static int calDist(Point a, Point b){
-        return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
-    }
+    public static void calDistance(int[] chickenIdxs){
+        int cityDist = 0; // 도시의 치킨 거리
+        for(int i=0; i<houseList.size(); i++){ // 집을 돌면서
+            Point house = houseList.get(i);
+            int houseDist = Integer.MAX_VALUE; // 해당 집의 치킨 거리
+            for(int j=0; j<M; j++){ // 선택한 M개의 치킨집 돌면서
+                Point chicken = chickenList.get(chickenIdxs[j]);
 
+                int dist = Math.abs(house.x-chicken.x) + Math.abs(house.y-chicken.y);
+
+                houseDist = Math.min(dist, houseDist);
+            }
+            cityDist += houseDist;
+        }
+        ans = Math.min(cityDist, ans);
+    }
 }
