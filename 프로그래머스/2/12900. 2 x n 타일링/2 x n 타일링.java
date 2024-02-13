9ef5105499f3b95ade11e4aef0f7f2cbc860496c
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2024-02-13 21:35:50 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.25 ms, Memory: 60.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        
+        int[] d = new int[60001];
+        d[1] = 1;
+        d[2] = 2;
+        for(int i=3; i<=n; i++){
+            d[i] = (d[i-1] + d[i-2]) % 1000000007;
+        }
+        
+        return d[n];
+    }
+}
